fa924dd51780fccbe8e7e2a9c0dba45afae51d51
Matthew Noyes <matthewnoyes@Matthews-MacBook.local>
2020-05-23 11:58:00 -0400

    Added back in necessary JSON files

--- build_app.sh
+++ build_app.sh
@@ -1,2 +1,8 @@
-./downloadImages.sh
-cd client && npm install
+#./downloadImages.sh
+
+GAME_DIR=$(pwd)
+
+cd $GAME_DIR/server && python3 -m venv venv
+
+
+#cd client && npm install
--- run_web_server.sh
+++ run_web_server.sh
@@ -3,6 +3,8 @@ trap "kill 0" EXIT
 
 GAME_DIR=$(pwd)
 
+source $GAME_DIR/server/venv/bin/activate
+
 cd $GAME_DIR/server && python app.py > $GAME_DIR/server.log &
 cd $GAME_DIR/client && npm run serve > $GAME_DIR/client.log &
 
