935abe2dc7e4fa7672ce7a3e98bb048e7cd0fb9d
Matthew Noyes <matthewnoyes@Matthews-MacBook.local>
2020-05-22 20:05:41 -0400

    Added logging to web server

--- run_web_server.sh
+++ run_web_server.sh
@@ -3,7 +3,7 @@ trap "kill 0" EXIT
 
 GAME_DIR=$(pwd)
 
-cd $GAME_DIR/server && python app.py &
-cd $GAME_DIR/client && npm run serve &
+cd $GAME_DIR/server && python app.py > $GAME_DIR/server.log &
+cd $GAME_DIR/client && npm run serve > $GAME_DIR/client.log &
 
 wait
